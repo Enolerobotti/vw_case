72248dae76d4dc45fdd1aeb5c21ff5ebea5de0ba
Artem Pavlovskii <enolerobotti.py@gmail.com>
2020-10-18 19:36:44 +0300

    Added python script for animation of the case

---
+++ postprocess.py
@@ -0,0 +1,29 @@
+import sys
+import paraview.simple as pv
+
+
+def animate(fn):
+    casefoam = pv.OpenFOAMReader(FileName=fn)
+    pv.Show(casefoam)
+    dp = pv.GetDisplayProperties(casefoam)
+    dp.SetPropertyWithName('ColorArrayName', ['POINTS', 'U'])
+    view=pv.GetActiveView()
+    reader = pv.GetActiveSource()
+    tsteps = reader.TimestepValues
+    annTime = pv.AnnotateTimeFilter(reader)
+    pv.Show(annTime)
+    pv.Render()
+    while True:
+        try:
+            for t in tsteps:
+                view.ViewTime = t
+                pv.Render()
+        except KeyboardInterrupt:
+            sys.exit(0)
+
+if __name__ == '__main__':
+    filename = [
+        "./thin_plate_case/thin_plate_case.foam",
+        "./thin_plate_case/thick_plate_case.foam"
+    ]
+    animate(filename[0])
